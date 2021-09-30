4a4cb5522dd59e5d8ffc09471a5957312f12b47a
gail enstice <skyedoll@aol.com>
2021-09-29 22:38:39 -0400

    wrote routes for posting and deleting comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
+
+//server connection
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-
+//path for the server
 router.use('./api', apiRoutes);
 router.use('./dashboard',dashboardRoutes);
 router.use((req,res)=> {
