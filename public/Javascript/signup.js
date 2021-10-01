25bebf5e41c12e202ad474f0c6ae59ba2ff677ca
gail enstice <skyedoll@aol.com>
2021-10-01 13:05:47 -0400

    sign up function

---
+++ public/Javascript/signup.js
@@ -0,0 +1,33 @@
+//Sign up form handler
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+        
+    //get info from the sign up form
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if(username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+
+        });
+        if (response.ok) {
+            alert('Account created!');
+            document.location.replace('/dashboard');
+
+        }else{
+            alert(response.statusText)
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit',signupFormHandler);
