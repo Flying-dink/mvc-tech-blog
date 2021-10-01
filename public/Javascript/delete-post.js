6c54bb2a006f2b4032fe9c4bc6131b182a07763e
gail enstice <skyedoll@aol.com>
2021-09-30 23:42:26 -0400

    delete form handler

---
+++ public/Javascript/delete-post.js
@@ -0,0 +1,24 @@
+//a function to delete a post
+
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+
+    //get the post id from the url
+    const id =window.location.toString().split('/')[
+        window/location.toString().split('/').length -1
+    ];
+    //delete the post function
+    const response = await fetch(`/api/posts/${id}`, {
+     method: 'DELETE'
+    });
+    //if delete is successful, then redirectto dashboard otherwise display error
+    if (response.ok) {
+        document.location.replace('/dashboard');
+
+    }else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
